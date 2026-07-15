1c6914b944e38595d2b8f692247ba33530e0fa42
Prathmesh Shinde <sprathmesh122002@gmail.com>
2026-07-16 00:18:26 +0530

    Add files via upload

---
+++ Core Python/Assignements/Assignement3/que3.py
@@ -0,0 +1,12 @@
+a1=int(input('Enter a first angle:'));
+a2=int(input('Enter a second angle:'));
+a3=int(input('Enter a third angle:'));
+
+sum=(a1+a2+a3);
+
+if(sum == 180):
+    print("it's Triangle");
+else:
+    print("It's not triangle");
+
+
